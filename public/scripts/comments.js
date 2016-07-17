421a0676bd7cc75d7e6746a29eaeb375af1c6b96
MisaOgura <misa.ogura01@gmail.com>
2016-07-16 17:09:45 -0700

    Adding markup using remarkable library but p is inserted

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,11 +1,12 @@
 var Comment = React.createClass({
   render: function() {
+    var md = new Remarkable();
     return (
       <div className="comments">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <h4> {md.render(this.props.children.toString())} </h4>
       </div>
     );
   }
